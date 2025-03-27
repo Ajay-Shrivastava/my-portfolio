46ca47f53d09b661fa3452e2f69a21652d6ddb02
Ajay Shrivastava <shrivastavaajay19@gmail.com>
2025-03-28 00:03:47 +0530

    404 redirect

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,13 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
+// Check for a stored route (redirected from 404.html)
+const storedPath = sessionStorage.getItem('redirectPath');
+if (storedPath) {
+  sessionStorage.removeItem('redirectPath'); // Remove it after use
+  sessionStorage.removeItem('redirected'); // Allow future redirects
+  history.replaceState({}, '', storedPath); // Restore correct URL
+}
+
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
