85031f2cb36affa78cd5d3af5ce100ffeb19433d
Ajay Shrivastava <shrivastavaajay19@gmail.com>
2025-03-28 00:36:26 +0530

    removed valid paths

--- src/main.ts
+++ src/main.ts
@@ -4,25 +4,11 @@ import { AppComponent } from './app/app.component';
 
 // Check for a stored route (redirected from 404.html)
 const storedPath = sessionStorage.getItem('redirectPath');
-const validPaths = [
-  'home',
-  'about',
-  'projects',
-  'concepts',
-  'blogs',
-  'contact'
-];
 
 if (storedPath) {
   sessionStorage.removeItem('redirectPath'); // Remove it after use
   sessionStorage.removeItem('redirected'); // Allow future redirects
-  var storedPathArray = storedPath.split('/');
-  if( validPaths.includes(storedPathArray[1])) {
-    history.replaceState({}, '', storedPath); // Restore correct URL
-  }
-  else {
-    history.replaceState({}, '', '/my-portfolio/home'); // Redirect to home if invalid path
-  }
+  history.replaceState({}, '', storedPath); // Restore correct URL
 }
 
 bootstrapApplication(AppComponent, appConfig)
